c6d03de9a192ca71ed2af5cea5a58c16d2d12bf4
Freshtuna <jun17114@gmail.com>
2021-02-19 14:10:13 +0900

    Add: room

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ const io = require('socket.io')(http);
 
 app.set('view engine','ejs');
 
+const rooms = { name :{}}
+
 app.get('/', (req,res) =>{
-    res.render('chat');
+    res.render('chat',{ rooms:rooms });
 });
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomName: req.params.room })
+})
+
+app.post('/room', (req,res) => {
+    rooms[req.body.room] = {users: {}};
+    res.redirect(req.body.room)
+})
 
 io.on('connection', (socket) => {
     socket.on('enterance-message',(nickname) => {
@@ -20,6 +31,18 @@ io.on('connection', (socket) => {
     socket.on('chat-message', (msg) => {
         socket.broadcast.emit('chat-message',msg);
     });
+
+    socket.on('type-message', (nickname) => {
+        console.log("focused:",nickname);
+        
+        socket.broadcast.emit('typing-notifiaction', nickname);
+    })
+
+    socket.on('type-message-erase', () => {
+        console.log("send-erase");
+        socket.broadcast.emit('erase-notification',"hi");
+        // console.log("sended");
+    })
 });
 
 http.listen(3000 , () => {
